f70849f8422499e9133b8c8ea0e64e9c3cfe866e
RubenCorona24 <a01803409@tec.mx>
2025-01-28 16:16:05 -0600

    cambio

--- practica01.py
+++ practica01.py
@@ -1,4 +1,11 @@
-a = 10
-b = 19
-resultado = a + b
-print(resultado)
+# Calcular el promedio de iMC
+peso =  float(input("introduce tu peso: "))
+estatura = float(input('introduce tu estatura en centimetros: '))
+alturaf = estatura*estatura
+imc = peso/alturaf
+print('tu imc es de', imc)
+if imc <= 18.5:
+    print("tu imc es muy bajo")
+elif imc > 25:
+    print("tu imc es muy alto")
+
