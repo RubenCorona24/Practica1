3b61d3bf5d38a7f8a015919cf472f23d338a7ac9
Ruben Santiago <coronamoronarubensantiago@gmail.com>
2025-06-20 16:51:02 -0600

    Versión actualizada juego.py

--- juego.py
+++ juego.py
@@ -10,8 +10,36 @@ print("***QUE EMPIEZE EL JUEGO***")
 print("INSTRUCCIONES \n Debes de completar todas las misiones sin excepción, al finalizar tu recompensa \n se te dara con un total de 100 monedas")
 
 
-vidas = 3
-while vidas:
-    pass
+
+#Estos son algunas notas para las modificaciones para un mejor entendimiento del juego
+
+class Jugador:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    
+    def correr(self):
+        print(f"Jugador {self.name} corriendo")
+    def comer(self):
+        print("El jugador está comiendo")
+    def __str__(self):
+        return f"Este es un juador llamado {self.name}, tiene {self.age} años y está adentrándose en un bosque profundo y peligroso."
+
+class Depredador:
+    def __init__(self,name,habilidad,vida):
+        self.name = name
+        self.habilidad = habilidad
+        self.vida = vida
+    def cazar(self):
+        print("El depredador esta cazando")
+    def atraer(self):
+        print("El depredador está atrayendo a la víctima")
+    def __str__(self):
+        return f"Este depredador llamado {self.name} busca víctimas entre el bosque, tiene la mortal habilidad de {self.habilidad}."
+
+Jugador1 = Jugador('Andy',18)
+depredador = Depredador('Lobo','Garras filosas',180)
+print(Jugador1)
+print(depredador)
 
 
