bace10199a7cd3576602759d806fd61597c2292f
Ruben Santiago <coronamoronarubensantiago@gmail.com>
2025-06-20 16:29:06 -0600

    Primer juego .py agregado

--- juego.py
+++ juego.py
@@ -1,1 +1,9 @@
-import pygame
+name = input("Dime tu nombre: ")
+print(f"Bien jugador, tu nombre es: {name}")
+num = randint(1,101)
+print(f"jugador {name}, tu número seleccionado es: {num}")
+area = ['parque','espacio','vecindario','escuela','hospital','bosque']
+area_seleccionada = choice(area)
+print(f"Jugador {name}, hemos seleccionado tu mapa de juego, te tocó : {area_seleccionada}")
+print("***QUE EMPIEZE EL JUEGO***")
+
