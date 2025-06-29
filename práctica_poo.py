22903dd765c767e0091bdd66f8faaa353671e755
Ruben Santiago <coronamoronarubensantiago@gmail.com>
2025-06-28 21:54:17 -0600

    Práctica_poo: Updated changes

--- práctica_poo.py
+++ práctica_poo.py
@@ -1,20 +1,44 @@
-from abc import ABC, abstractclassmethod
+from abc import ABC, abstractmethod
+
 
 class SerVivo(ABC):
-    def __init__(self,age,name,height):
+    def __init__(self, age, name, height):
         self.age = age
         self.name = name
         self.height = height
-    @abstractclassmethod
+
+    @abstractmethod
     def born(self):
-        print("The living being was born")
-    @abstractclassmethod
+        pass
+
+    @abstractmethod
     def breathe(self):
-        print("The living being is breathing")
+        pass
 
-    @abstractclassmethod
+    @abstractmethod
     def eat(self):
-        print("The living being is eating")
+        pass
 
-living_being = SerVivo(19,'Juan','170cm')
-print(living_being)
+    def __str__(self):
+        return f"Age: {self.age} Name: {self.name} Height: {self.height}"
+
+
+class Human(SerVivo):
+    def __init__(self, age, name, height):
+        super().__init__(age, name, height)
+
+    def born(self):
+        print("The human was born")
+
+    def breathe(self):
+        print("The human is breathing")
+
+    def eat(self):
+        print("The human is eating")
+
+    def presentarse(self):
+        print(f"Hello my name is {self.name}, and I'm {self.age} years old")
+
+human1 = Human(14, 'Sam', '160cm')
+human1.presentarse()
+print(human1)
