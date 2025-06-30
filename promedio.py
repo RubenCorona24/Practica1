8460d8c79ae6bd4cf8af8b70ac85b1aec7d418ee
Ruben Santiago <coronamoronarubensantiago@gmail.com>
2025-06-30 12:49:22 -0600

    Changes

--- promedio.py
+++ promedio.py
@@ -1,15 +1,33 @@
 # CALCULAR PRIMEDIO 
 print("Bienvenidos a este sistema para calcular tu promedio de las materias")
-nombre = str(input("¿cuál es tu nombre? "))
-calif_esp = float(input("¿cual es tu calificacion de esp? "))
-calif_mate = float(input("¿Cuál es tu calificación de matemáticas? "))
-calif_historia = float(input("¿Cuál es tu calificación de historia? "))
+nombre = str(input("¿cuál es tu nombre?: "))
+calif_esp = float(input("¿cual es tu calificacion de esp?: "))
+calif_mate = float(input("¿Cuál es tu calificación de matemáticas?: "))
+calif_historia = float(input("¿Cuál es tu calificación de historia?: "))
 promedio = (calif_esp + calif_mate + calif_historia) / 3
-if promedio == 10:
-    print("excelente")
-elif promedio >= 9:
-    print("Felicidades")
-elif promedio >= 8:
-    print("Bien")
-else:
-    print("reprobaste")
+
+def probar_promedio(promedio):
+
+
+    if promedio == 10:
+     print("Excelente, tuviste promedio perfecto")
+    elif promedio >= 9:
+            print("Felicidades, sigue así")
+    elif promedio >= 8:
+        print("Bien, pero necesitas mejorar más")
+    else:
+        print("Reprobaste, tienes que hacer extraordinarios")
+try:
+    probar_promedio(promedio)
+except:
+    print("incorrect data provided")
+finally:
+    print("Finished Process")
+
+
+
+
+
+
+
+
