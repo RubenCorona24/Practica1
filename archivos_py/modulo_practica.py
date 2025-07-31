6a64ce79f9b7c9560865bc40e0a48ac90817fa60
Ruben Santiago <coronamoronarubensantiago@gmail.com>
2025-07-31 16:54:16 -0600

    Modified modules

--- archivos_py/modulo_practica.py
+++ archivos_py/modulo_practica.py
@@ -1,5 +1,6 @@
-import modulo_presentar as mp
-while True:
+import modulo_presentar as mp #Importamos otro módulo, con el fin de no volver a escribir las funciones
+
+while True:  #Entramos en bucle
     try:
         nombre = input("Dime tu nombre completo: ")
         if nombre.isnumeric():
@@ -13,7 +14,7 @@ while True:
         if empresa.isnumeric():
             print("Resultado mal dado")
         print("Estamos accediendo a tus datos y formando tu curriculum")
-        print(mp.presentar(nombre,pais,empresa))
+        print(mp.presentar(nombre,pais,empresa))  #Imprime los datos
         
     except:
         print("Datos Inválidos")
