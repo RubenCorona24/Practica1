8800a21a8bcf105957b49097b868b9f3d51ed4c9
Ruben Santiago <coronamoronarubensantiago@gmail.com>
2025-08-04 13:57:57 -0600

    Added gráfico lineal

---
+++ archivos_py/archivos_graficos/grafico_lineal.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt #visualización de datos
+import seaborn as sns #Gráficos estadísticos #importamos las librerías
+
+df = pd.read_csv('archivos_aparte\\tareas.csv')
+print(df) #leído correctamente
+sns.lineplot(x='fecha',y='tareas',data=df) #hacer el gráfico con las coordenadas, y el dataframe
+plt.plot("01-06",6,'o') #Marcar un punto en el gráfico
+plt.show() #Mostramos el gráfico
+
+
