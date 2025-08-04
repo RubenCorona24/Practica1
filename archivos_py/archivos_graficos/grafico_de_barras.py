d0037ba8e56b759be2a36d762aab8afdba33ddcd
Ruben Santiago <coronamoronarubensantiago@gmail.com>
2025-08-04 14:21:54 -0600

    Modified graphics

--- archivos_py/archivos_graficos/grafico_de_barras.py
+++ archivos_py/archivos_graficos/grafico_de_barras.py
@@ -4,7 +4,7 @@ import seaborn as sns #Gráficos estadísticos #importamos las librerías
 #Un gráfico de barras mediante la info. de el archivo fuente e ingresos
 df = pd.read_csv('archivos_aparte\\ingresos.csv')
 
-
+##sns.barplot() barras
 sns.barplot(x='fuente',y='ingresos',data=df) #hacer el gráfico con las coordenadas, y el dataframe
 total_ingresos = df['ingresos'].sum() #Obteniendo el total de ingresos
 print(f"El total de ingresos es: {total_ingresos}USD") #Mostrar el total de ingresos
--- archivos_py/archivos_graficos/grafico_lineal.py
+++ archivos_py/archivos_graficos/grafico_lineal.py
@@ -4,6 +4,7 @@ import seaborn as sns #Gráficos estadísticos #importamos las librerías
 
 df = pd.read_csv('archivos_aparte\\tareas.csv')
 print(df) #leído correctamente
+##sns.lineplot() lineal
 sns.lineplot(x='fecha',y='tareas',data=df) #hacer el gráfico con las coordenadas, y el dataframe
 plt.plot("01-06",6,'o') #Marcar un punto en el gráfico
 plt.show() #Mostramos el gráfico
