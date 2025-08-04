1a9748129f1d9fd2e33afdd08d976d92a4ef34d1
Ruben Santiago <coronamoronarubensantiago@gmail.com>
2025-08-04 14:21:54 -0600

    Modified graphics

--- archivos_py/archivos_graficos/gráfico_de_dispersión.py
+++ archivos_py/archivos_graficos/gráfico_de_dispersión.py
@@ -5,6 +5,6 @@ import seaborn as sns #Gráficos estadísticos #importamos las librerías
 #Un gráfico de barras mediante la info. de el archivo fuente e ingresos
 df = pd.read_csv('archivos_aparte\\dispersion.csv')
 
-
+##sns.scatterplot() dispersión
 sns.scatterplot(x='tiempo',y='dinero',data=df) #hacer el gráfico de dispersión, y el dataframe
 plt.show() #Mostramos el gráfico
