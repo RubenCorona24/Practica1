b310d9667f9a825b80c1140b313b0018e40baa8c
Ruben Santiago <coronamoronarubensantiago@gmail.com>
2025-09-15 17:26:58 -0600

    Added data cleaning

--- archivos_py/funciones/funciones_dentro_funciones.py
+++ archivos_py/funciones/funciones_dentro_funciones.py
@@ -57,5 +57,16 @@ def hacer_tabla():
             break
         else:
             print("No has dado una respuesta clara  ??")
-hacer_tabla()
 
+
+
+import pandas as pd
+datos = {"nombre":["Ana","Luis","Carlos"],"edad":[30,25,40]}
+df_empleados = pd.DataFrame(datos)
+print(df_empleados)
+shape_df = df_empleados.shape
+columns_df = df_empleados.columns
+index_df = df_empleados.index
+print(shape_df)
+print(columns_df)
+print(index_df)
