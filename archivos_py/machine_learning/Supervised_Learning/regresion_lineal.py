f4387bde975e2166176406c0b108598e8066b03d
Ruben Santiago <coronamoronarubensantiago@gmail.com>
2025-11-05 17:40:42 -0600

    Modified Linear Regression

--- archivos_py/machine_learning/Supervised_Learning/regresion_lineal.py
+++ archivos_py/machine_learning/Supervised_Learning/regresion_lineal.py
@@ -1,6 +1,11 @@
+#Regresión Lineal: lgoritmo de aprendizaje supervisado que se utiliza para predecir un valor continuo (variable dependiente) basándose en una o más variables independientes. 
+#Se basa en ajustar una línea recta que mejor se adapte a los datos, minimizando el error entre los valores predichos y reales,
+#con el objetivo de modelar la relación entre las variables. 
+
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn import linear_model #Importamos linear model desde sklearn
+import random
 #Creamos un df (columna precio y columna area)
 df = pd.DataFrame({
     'Area': [26000,30000,32000,36000,40000],
@@ -27,3 +32,28 @@ plt.title("Gráfica con modelo entrenado")
 plt.xlabel("Area de la casa")
 plt.ylabel("Precio ($)")
 plt.show()
+
+#Otroo ejemplo de regresión lineal
+#Predicción del Precio de una casa según su tamaño
+df = pd.DataFrame({
+    'Tamaño': [50,60,80, 100,120],
+    'Precio(USD)': [150000,180000,240000,300000,360000]
+})
+
+def entrenar_modelo():
+    X2 = df[['Tamaño']]
+    y2 = df['Precio(USD)']
+    model = linear_model.LinearRegression()
+    model.fit(X2,y2) #Entrenamos al modelo
+    size_aleatorio = random.randint(120,200)
+    print(f"Costo del tamaño de {size_aleatorio}m²: {model.predict([[size_aleatorio]])[0]:.2f}")
+    return X2,y2,model
+
+X2,y2,model = entrenar_modelo()
+def graficar_modelo(X,y):
+    plt.scatter(X,y,color='blue',marker='o',label='Data Real') 
+    plt.plot(X,model.predict(X),color='red',marker='+',label='Model predictions')
+    plt.xlabel('Tamaño en Metros cuadrados')
+    plt.ylabel("Precio (USD)")
+    plt.show()   
+graficar_modelo(X2,y2) #Llamamos a la función de graficar el modelo
