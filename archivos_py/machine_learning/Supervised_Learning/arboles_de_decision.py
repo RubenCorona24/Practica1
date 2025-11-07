46e8f5d6795dbb109114eb34d398e4282619fc07
Ruben Santiago <coronamoronarubensantiago@gmail.com>
2025-11-07 17:52:25 -0600

    Modified Decision Tree

--- archivos_py/machine_learning/Supervised_Learning/arboles_de_decision.py
+++ archivos_py/machine_learning/Supervised_Learning/arboles_de_decision.py
@@ -17,7 +17,7 @@ le = LabelEncoder()
 y = le.fit_transform(df.species)
 
 #Separamos con train_test_split
-X_entrena,y_entrena,X_prueba,y_prueba = train_test_split(X,y,test_size=0.8,random_state=42)
+X_entrena,X_prueba,y_entrena,y_prueba = train_test_split(X,y,test_size=0.8,random_state=42)
 #train_size = 0.8: que el tamaño de la muestra sea igual al 80 porciento(entrenamiento), 20 porciento a la prueba
 #random_state: controla la aleatoriedad que se distribuyen los registros, en este caso, la division que se haga de los datos cada que se reproduzca, será siempre la misma
 
@@ -26,11 +26,15 @@ arbol =  DecisionTreeClassifier()
 arbol.fit(X_entrena,y_entrena) #Entrenamos al modelo
 
 #Visualizamos el arbol con plot, con los argumentos necesarios
-plt.figure(figsize=(20,20))
+plt.figure(figsize=(8,8))
 plot_tree(decision_tree=arbol,
           filled=True,
            class_names=['setosa','versicolor','virginica'],
            feature_names=df.columns.to_list())
+plt.title("ÁRBOL DE DECISIÓN DE CLASES DE PLANTAS")
+plt.savefig("archivos_aparte/data_sns/arbol_de_decision_plantas.png")
+plt.show() #Mostramos el modelo
+
 #Filled = True: rellena en colores por clase
 #class_names: agrega nombre a cada clase
 #feature_names: visualizamos las columnas por su nommbre en vez de la indexación
