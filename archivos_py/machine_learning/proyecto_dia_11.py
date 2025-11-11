5e1379b7e0777067ba1ebc91130de89ff4d2cdc1
Ruben Santiago <coronamoronarubensantiago@gmail.com>
2025-11-11 07:55:05 -0600

    Modified project 11

--- archivos_py/machine_learning/proyecto_dia_11.py
+++ archivos_py/machine_learning/proyecto_dia_11.py
@@ -1,1 +1,5 @@
 #PROYECTO DÍA 11
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeClassifier,plot_tree
+from sklearn.ensemble import  RandomForestClassifier
