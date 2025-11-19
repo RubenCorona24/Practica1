e4f5eb64327758ebb810d367e7870093f86fa4c0
Ruben Santiago <coronamoronarubensantiago@gmail.com>
2025-11-18 18:19:30 -0600

    Added kmeans project shopping

--- archivos_py/machine_learning/Unsupervised learning/PCA_analisis_componentes.py
+++ archivos_py/machine_learning/Unsupervised learning/PCA_analisis_componentes.py
@@ -10,8 +10,7 @@ X = iris.data #Los datos del dataset
 y = iris.target #La parte del target
 #Estandarizamos/normalizamos los datos
 scale = StandardScaler() 
-X_scaler = scale.fit_transform(X)
-#Creamos al modelo PCA
+X_scaler = scale.fit_transform(X)#Creamos al modelo PCA
 pca = PCA(n_components=2)#Definimos que trabajaremos con 2 componentes
 X_pca = pca.fit_transform(X_scaler) #Entrenamos al modelo pasando datos normalizados
 #Visualizamos los datos
