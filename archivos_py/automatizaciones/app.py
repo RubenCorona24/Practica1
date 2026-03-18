d6ff5a5adff5b7a9deab06c02447b2d2b40724e1
Ruben Santiago <coronamoronarubensantiago@gmail.com>
2026-03-17 20:03:23 -0600

    Added streamlit projects

---
+++ archivos_py/automatizaciones/app.py
@@ -0,0 +1,9 @@
+#Importamos librerías necesarias
+import streamlit as st
+
+def main():
+    st.title('Página Data Science') #Crea un título tipo h1 de html
+    st.text("Esto es un texto de introducción")
+
+if __name__ == '__main__':
+    main()
