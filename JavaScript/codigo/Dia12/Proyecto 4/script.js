0f9b40b08b0eff05dee2201db2b50e28121208b9
Ruben Santiago <coronamoronarubensantiago@gmail.com>
2026-05-20 16:43:54 -0600

    Project API Countrys v2.0

--- JavaScript/codigo/Dia12/Proyecto 4/script.js
+++ JavaScript/codigo/Dia12/Proyecto 4/script.js
@@ -4,18 +4,23 @@ let boton = document.getElementById("btnBuscar")
 let conteendor = document.getElementById("contenedor")
 
 //evento de input
-input.addEventListener("keydown",function(e){
-    if (e.keyCode < 31 || e.keyCode > 128 ){
+
+input.addEventListener("keydown", function(e) {
+    if (e.keyCode < 31 || e.keyCode > 128) {
         if (e.key === "Backspace" || e.key === "Delete") {
-            return  // ← dejar pasar
-            }else{
-                e.preventDefault(); //Evita el evento por defecto
-            }
+            return
+        } else {
+            e.preventDefault()
+        }
     }
 })
 
 //evento de boton
 boton.addEventListener("click",function(e){
+    if (input.value.trim() === "") {
+        alert("⚠️ Escribe el nombre de un país")
+        return
+    }
     conteendor.innerHTML = ""
     let pais = input.value
     fetch("https://restcountries.com/v3.1/name/"+pais)
@@ -29,7 +34,7 @@ boton.addEventListener("click",function(e){
         let continente = document.createElement("p")
         continente.textContent = "Continente: "+data[0].continents
         let imagen = document.createElement("img")
-        imagen.setAttribute("src",pais+".png")
+        imagen.setAttribute("src",data[0].flags.png)
         conteendor.appendChild(nombre)
         conteendor.appendChild(capital)
         conteendor.appendChild(continente)
@@ -37,6 +42,51 @@ boton.addEventListener("click",function(e){
         let descripcion = document.createElement("h2")
         descripcion.textContent = "Descripción General"
         conteendor.appendChild(descripcion)
+        let p = document.createElement("p")
+        let mensaje;
+        if (data[0].independent === true){
+            mensaje = "Estado Independiente"
+            p.style.color = "green"
+        }else{
+            mensaje = "Estado No Independiente"
+            p.style.color = "red"
+        }
+        p.textContent = mensaje
+        p.setAttribute("id","estado")
+        conteendor.appendChild(p)
+        let poblacion = document.createElement("p")
+        poblacion.textContent = "Población: "+data[0].population.toLocaleString()
+        conteendor.appendChild(poblacion)
+        let idiomas = Object.values(data[0].languages) //objeto
+        let idioma = document.createElement("p")
+        idioma.textContent = "Idiomas: "+idiomas.join(", ") //utilizamos método join con separador ", "
+        conteendor.appendChild(idioma)
+        let area = document.createElement("p")
+        area.textContent = "Área: "+data[0].area.toLocaleString()+" km²"
+        conteendor.appendChild(area)
+        //evento de mouseover en descripcion
+        descripcion.addEventListener("mouseover",function(){
+            p.style.display = "block";
+            poblacion.style.display = "block";
+            idioma.style.display = "block";
+            area.style.display = "block";
+        })
+        descripcion.addEventListener("mouseout",function(){
+            p.style.display = "none";
+            poblacion.style.display = "none";
+            idioma.style.display = "none";
+            area.style.display = "none";
+        })
+        p.style.display = "none";
+        poblacion.style.display = "none";
+        idioma.style.display = "none";
+        area.style.display = "none";
 
+
+        
+
+    })
+    .catch(error =>{
+        conteendor.innerHTML = "<p style='color:red'>⚠️ País no encontrado</p>" //en caso de país no encontrado
     })
 })
